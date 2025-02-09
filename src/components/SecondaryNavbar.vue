e5fda7169d65ccb058ebce92e11658c8d3bd2b41
SamuelYongMeo <samuel.yong@devsquad.my>
2025-02-09 14:47:55 +0800

    removed the 'absolute' trait from the avatar's class and used flexbox instead

--- src/components/SecondaryNavbar.vue
+++ src/components/SecondaryNavbar.vue
@@ -4,12 +4,14 @@
 
 <template>
   <!-- User Profile (Blue Navbar) -->
-  <div class="z-10 bg-[#2596be] text-white text-sm p-3 w-full fixed top-16 left-0 flex flex-col space-y-2 min-h-[200px]">
-    <!-- Avatar -->
-    <img :src="avatar" alt="Logo" class="absolute left-5 w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px]">
-    
+<div class="z-10 bg-[#2596be] text-white text-sm p-6 w-full fixed top-16 left-0 flex items-center gap-6 min-h-[200px]">
+  <!-- Avatar -->
+  <img :src="avatar" alt="Avatar" class="w-[150px] h-[150px]">
+
+  <!-- Content (Buttons & User Info) -->
+  <div class="flex flex-col space-y-3">
     <!-- Buttons -->
-    <div class="w-full flex flex-wrap justify-start items-center gap-4 max-w-7xl mx-auto pl-20">
+    <div class="flex flex-wrap items-center gap-4">
       <button class="bg-white hover:text-white hover:bg-[#2596be] border-2 border-white text-black font-bold py-2 px-4 rounded-full">
         <span class="pi pi-send"></span> Send Message
       </button>
@@ -19,11 +21,13 @@
     </div>
 
     <!-- User Info -->
-    <div class="text-2xl sm:text-3xl md:text-4xl font-semibold w-full flex justify-start items-center max-w-7xl mx-auto pl-20">
+    <div class="text-2xl sm:text-3xl md:text-4xl font-semibold">
       John Doe
     </div>
-    <div class="w-full flex justify-start items-center max-w-7xl mx-auto pl-20">
+    <div>
       Last online: 2 days ago
     </div>
   </div>
+</div>
+
 </template>
